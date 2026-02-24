6afa439590f9c9682f89781b0d080ac6b8f8fe09
Atanas Tumbalev <atanastumbalev@192.168.100.32>
2026-02-24 22:20:02 +0200

    Refactor content and layout. Add logo.

--- src/components/hero_page/HeroFooter.tsx
+++ src/components/hero_page/HeroFooter.tsx
@@ -3,16 +3,6 @@ import React from "react";
 const FooterLinks: React.FC = () => {
   return (
     <footer className="footer-nav">
-      <div className="footer-item">
-        Our Name <span>→</span>
-      </div>
-      <div className="footer-item">
-        Our People <span>→</span>
-      </div>
-      <div className="footer-item">
-        Our Mission <span>→</span>
-      </div>
-      <div className="footer-item active">Our Way</div>
     </footer>
   );
 };
--- src/components/hero_page/Nav.tsx
+++ src/components/hero_page/Nav.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import logo from '../../assets/21GRAMS logo.png';
 
 const Nav: React.FC = () => {
   return (
     <header className="nav">
-      <div className="nav-left">21GRAMS</div>
+      <div className="nav-left">
+        <img
+          src={logo}
+          alt="21GRAMS"
+          style={{ width: "150px", height: "auto" }}
+        />
+      </div>
 
       <nav className="nav-center">
         <a href="#">Who We Are</a>
         <a href="#">What We Do</a>
         <a href="#">Work With Us</a>
       </nav>
-
-      <div className="nav-right">
-        <button className="cta">Let's Have Dinner</button>
-      </div>
     </header>
   );
 };
